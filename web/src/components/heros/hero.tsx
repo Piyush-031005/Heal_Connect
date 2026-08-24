6785e1bc2031986b3625bc08026305b68d9051ca
Piyush-031005 <piyushpunera107@gmail.com>
2026-08-24 12:34:26 +0530

    UI Tweaks: Re-adjusted optical wheel size to match original HealConnect proportion, injected Zodiacs into optical wheel, and removed black background from icons

--- web/src/components/heros/hero.tsx
+++ web/src/components/heros/hero.tsx
@@ -33,7 +33,7 @@ function PrimaryHero() {
       </svg>
 
       {/* Big Modality Wheel - Scaled up and shifted right to create the Arc effect */}
-      <div className="absolute right-0 translate-x-[35%] md:translate-x-[45%] lg:translate-x-[55%] top-1/2 -translate-y-1/2 h-[500px] w-[500px] md:h-[600px] md:w-[600px] lg:h-[700px] lg:w-[700px] opacity-90 lg:opacity-100 z-10 pointer-events-none lg:pointer-events-auto flex items-center justify-center">
+      <div className="absolute right-0 translate-x-[25%] md:translate-x-[30%] lg:translate-x-[20%] top-1/2 -translate-y-1/2 h-[700px] w-[700px] md:h-[850px] md:w-[850px] lg:h-[1000px] lg:w-[1000px] opacity-90 lg:opacity-100 z-10 pointer-events-none lg:pointer-events-auto flex items-center justify-center">
         <OpticalWheel />
       </div>
 
