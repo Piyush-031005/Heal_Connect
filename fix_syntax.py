524f8a1cad1e0726139fc135892e5855b020dc68
Piyush-031005 <piyushpunera107@gmail.com>
2026-09-02 18:59:28 +0530

    chore: cleanup python scripts

--- fix_syntax.py
+++
@@ -1,9 +0,0 @@
-nav_path = "web/src/components/navbar.tsx"
-with open(nav_path, "r", encoding="utf-8") as f:
-    nav = f.read()
-
-# Fix syntax error from escaped quotes
-nav = nav.replace("\\'", "'")
-
-with open(nav_path, "w", encoding="utf-8") as f:
-    f.write(nav)
